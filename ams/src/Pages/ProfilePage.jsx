f19f62864a59d5f5de01b594f67c273e1fe68cdc
YashRahate <156834748+YashRahate@users.noreply.github.com>
2024-12-29 15:18:32 +0530

    Update ProfilePage.jsx

--- ams/src/Pages/ProfilePage.jsx
+++ ams/src/Pages/ProfilePage.jsx
@@ -21,7 +21,7 @@
 //   useEffect(() => {
 //     const getOutlets = async () => {
 //       try {
-//         const response = await axios.get("http://localhost:5000/api/outlet");
+//         const response = await axios.get("https://palcoaintegration-backend.onrender.com/api/outlet");
 //         const outletOptions = response.data.map(outlet => ({
 //           value: outlet._id,
 //           label: outlet.outlet_name
@@ -164,7 +164,7 @@ export default function ProfilePage() {
   useEffect(() => {
     const getOutlets = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/api/outlet");
+        const response = await axios.get("https://palcoaintegration-backend.onrender.com/api/outlet");
         const outletOptions = response.data.map((outlet) => ({
           value: outlet._id,
           label: outlet.outlet_name,
