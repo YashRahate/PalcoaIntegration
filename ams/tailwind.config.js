02ffe90e00ae6a831f0ca7e1f1f237f7038fd05f
YashRahate <156834748+YashRahate@users.noreply.github.com>
2024-12-29 17:15:49 +0530

    Update tailwind.config.js

--- ams/tailwind.config.js
+++ ams/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default = {
+export default {
   darkMode: ["class"],
   content: [
     './pages/**/*.{js,jsx}',
@@ -74,4 +74,4 @@ export default = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
